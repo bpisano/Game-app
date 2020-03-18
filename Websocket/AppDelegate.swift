d6e80f4f2052475b25640b3b09eb9554007561c0
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-03-18 16:13:49 +0100

    Initial Commit

---
+++ Websocket/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Websocket
+//
+//  Created by Benjamin Pisano on 18/03/2020.
+//  Copyright © 2020 Snopia. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
